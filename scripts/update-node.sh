2b8c5670c0d3b55733afa76a8ff052fef850edaf
antonycc <antonyccartwright@gmail.com>
2026-03-28 18:01:26 +0000

    chore: update dependencies and add update-node script
    - Bump Java and Node dependencies to latest versions in pom.xml and package.json
    - Add update-to-greatest npm script for upgrading to greatest non-alpha versions
    - Add update-node.sh script to automate Node dependency updates and build steps

---
+++ scripts/update-node.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# scripts/update.sh
+# Usage: ./scripts/update-node.sh
+#
+
+rm -f package-lock.json
+rm -rf node-modules
+rm -rf cdk-spreadsheets.out
+npm install
+npm run update-to-greatest
+npm update
+npm upgrade
+npm install
+npm run build
+npm link
